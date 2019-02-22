54298c193690403ce2c4cdd42ae8911d904fca6f
iafine <iafineden@gmail.com>
2019-02-22 15:09:53 +0800

    synchronise repository

---
+++ webpack/demo05/main.js
@@ -0,0 +1,7 @@
+var img1 = document.createElement('img')
+img1.src = require('./empty.png')
+document.body.appendChild(img1)
+
+var img2 = document.createElement('img')
+img2.src = require('./finish.png')
+document.body.appendChild(img2)
