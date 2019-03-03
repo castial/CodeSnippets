222981f0c91fbfde01625ff2c3429eca93d2bdfd
iafine <iafineden@gmail.com>
2019-03-03 14:44:52 +0800

    synchronise repository

--- vue-element-demo/webpack.config.js
+++ vue-element-demo/webpack.config.js
@@ -1,7 +1,24 @@
+const path = require('path')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+
 module.exports = {
-    entry: './app/index.js',
+    entry: './app/app.js',
     output: {
         filename: 'bundle.js',
-        path: './dist'
+        path: path.resolve(__dirname, './dist')
+    },
+    module: {
+        rules: [
+            { test: /\.vue$/, loader: 'vue-loader' }
+        ]
+    },
+    plugins: [
+        new VueLoaderPlugin()
+    ],
+    devServer: {
+        contentBase: path.join(__dirname, './app'),
+        port: 8084,
+        open: true,
+        hot: true
     }
 }
