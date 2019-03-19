3372b33e505cfed0f524407049a35e9dd85529a7
iafine <iafineden@gmail.com>
2019-03-19 11:10:30 +0800

    synchronise repository

--- vue-demo/src/page/home/index.vue
+++ vue-demo/src/page/home/index.vue
@@ -1,5 +1,26 @@
 <template>
-    <div>
-        <h1>这个是home页面</h1>
+    <div class="home_container">
+        <section>
+            <header class="top_tips">
+                <span>{{ title }}</span>
+            </header>
+            <div>
+                <button type="button" @click="pushItem">开始做题</button>
+            </div>
+        </section>
     </div>
 </template>
+<script>
+export default {
+  data () {
+    return {
+      title: '点击下方，开始做题吧!'
+    }
+  },
+  methods: {
+    pushItem () {
+      this.$router.push('/item')
+    }
+  }
+}
+</script>
--- vue-demo/src/router/router.js
+++ vue-demo/src/router/router.js
@@ -13,30 +13,23 @@ export default new Router({
   routes: [
     {
       path: '/',
+      name: 'index',
+      component: Home
+    },
+    {
+      path: '/home',
       name: 'home',
-      component: Home,
-      children: [
-        {
-          path: '/home',
-          component: Home
-        },
-        {
-          path: '/item',
-          component: Item
-        },
-        {
-          path: '/score',
-          component: Score
-        }
-      ]
+      component: Home
+    },
+    {
+      path: '/item',
+      name: 'item',
+      component: Item
+    },
+    {
+      path: '/score',
+      name: 'score',
+      component: Score
     }
-    // {
-    //   path: '/about',
-    //   name: 'about',
-    //   // route level code-splitting
-    //   // this generates a separate chunk (about.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-    // }
   ]
 })
