f1cf7e55f6d7d49753f9d7c1e169e53a722c7540
iafine <iafineden@gmail.com>
2019-03-19 14:30:10 +0800

    synchronise repository

--- vue-demo/src/page/item/index.vue
+++ vue-demo/src/page/item/index.vue
@@ -1,5 +1,51 @@
 <template>
-    <div>
-        <h1>这个是item页面</h1>
+  <div class="item_container">
+    <div class="item_list">
+      <header class="item_title">
+          <span>{{ itemDetail.question }}</span>
+      </header>
+      <form>
+        <div class="item_radio" :key="item.id" v-for="item in itemDetail.answerList">
+          <input name="answer" type="radio" :value="item.id" v-model="picked">
+          {{ item.answer }}
+        </div>
+        <button type="button" @click="handleClicked">下一题</button>
+      </form>
     </div>
+  </div>
 </template>
+<script>
+export default {
+  data () {
+    return {
+      picked: '',
+      itemDetail: {
+        question: '请问世界上最大的湖泊是哪个?',
+        answerList: [
+          {
+            answer: '答案1',
+            id: 1
+          },
+          {
+            answer: '答案1',
+            id: 2
+          },
+          {
+            answer: '答案1',
+            id: 3
+          },
+          {
+            answer: '答案1',
+            id: 4
+          }
+        ]
+      }
+    }
+  },
+  methods: {
+    handleClicked () {
+      alert('选中的是' + this.picked)
+    }
+  }
+}
+</script>
--- vue-demo/src/page/score/index.vue
+++ vue-demo/src/page/score/index.vue
@@ -1,5 +1,11 @@
 <template>
-    <div>
-        <h1>这个是score页面</h1>
+    <div class="score_container">
+        <header>
+            <span>80分</span>
+        </header>
+        <div class="result_tip">
+            还不错，但是还需要继续努力哦！
+        </div>
+        <button type="button">重新开始</button>
     </div>
 </template>
