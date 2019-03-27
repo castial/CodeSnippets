6a5560933d0c8398269ca119f1dd3c22b758c283
iafine <iafineden@gmail.com>
2019-03-27 10:36:15 +0800

    synchronise repository

--- ios_framework/build/ios_framework.build/Release-iphonesimulator/ios_framework.build/Objects-normal/i386/ios_framework_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/harlan/Documents/Learn/code_snippets/ios_framework/build/ios_framework.build/Release-iphonesimulator/ios_framework.build/DerivedSources/ios_framework_vers.c
--- ios_framework/build/ios_framework.build/Release-iphonesimulator/ios_framework.build/Objects-normal/x86_64/HYAIManager.d
+++
@@ -1,5 +0,0 @@
-dependencies: \
-  /Users/harlan/Documents/Learn/code_snippets/ios_framework/ios_framework/Core/HYAIManager.m \
-  /Users/harlan/Documents/Learn/code_snippets/ios_framework/build/ios_framework.build/Release-iphonesimulator/ios_framework.build/module.modulemap \
-  /Users/harlan/Documents/Learn/code_snippets/ios_framework/ios_framework/Core/HYAIManager.h \
-  /Users/harlan/Documents/Learn/code_snippets/ios_framework/ios_framework/Vendor/AIPIFlyMSC.framework/Headers/AIPIFlySpeechUtility.h
