e2a9df4925235f07d944721f6183a1441c261055
iafine <iafineden@gmail.com>
2019-03-29 18:29:40 +0800

    synchronise repository

--- JavaScript/full-stack-framework/index.js
+++ JavaScript/full-stack-framework/index.js
@@ -7,48 +7,42 @@ const koaStatic = require('koa-static')
 
 const app = new Koa()
 
-runDevServer()
+runServer()
 
-async function runDevServer() {
-    // 引入路由中间件
-    app.use(routers.routes())
-    app.use(routers.allowedMethods())
-    
-    app.use(koaStatic('public'))    // 项目用到的静态资源放在这里
-    
-    // 引入前端路由
-    app.use(history({
-        htmlAcceptHeaders: ['text/html'],
-        index: '/'
-    }))
-
-
-    // 使用koa-webpack调试开发环境
-    const middleware = await koaWebpack({
-        config: webpackConfig,
-        devMiddleware: {
-            // stats: 'minimal'    // 只有在发生错误或者有新的编译的时候输出
-        }
-    })
-    app.use(middleware)
-
-    app.listen(8081)
-}
-
-async function runProdServer() {
+/**
+ * 启动服务
+ */
+async function runServer() {
     // 引入路由中间件
     app.use(routers.routes())
     app.use(routers.allowedMethods())
 
-    // 引入前端路由
-    app.use(history({
-        htmlAcceptHeaders: ['text/html'],
-        index: '/index.html'
-    }))
-
-    // 引入静态资源
-    app.use(koaStatic('vue-dist'))
     app.use(koaStatic('public'))    // 项目用到的静态资源放在这里
 
+    if (process.env.NODE_ENV === 'production') {
+        // 引入前端路由
+        app.use(history({
+            htmlAcceptHeaders: ['text/html'],
+            index: '/index.html'
+        }))
+
+        // 引入静态资源
+        app.use(koaStatic('vue-dist'))
+    } else {
+        // 引入前端路由
+        app.use(history({
+            htmlAcceptHeaders: ['text/html'],
+            index: '/'
+        }))
+
+        // 使用koa-webpack调试开发环境
+        const middleware = await koaWebpack({
+            config: webpackConfig,
+            devMiddleware: {
+                // stats: 'minimal'    // 只有在发生错误或者有新的编译的时候输出
+            }
+        })
+        app.use(middleware)
+    }
     app.listen(8081)
 }
